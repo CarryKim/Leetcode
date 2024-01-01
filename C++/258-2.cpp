534b596bb7a9bae46f686e8062499fdc76394687
CarryKim <137850094+CarryKim@users.noreply.github.com>
2024-01-02 02:07:29 +0900

    258-2.cpp

---
+++ C++/258-2.cpp
@@ -0,0 +1,19 @@
+// 258. Add Digits
+// https://leetcode.com/problems/add-digits/
+// Authored by CarryKim
+
+class Solution {
+public:
+    int addDigits(int num) {
+        int ans = 0;
+        while(num >= 10) {
+            ans = 0;
+            while(num) {
+                ans += num%10;
+                num /= 10;
+            }
+            num = ans;
+        }
+        return num;
+    }
+};
