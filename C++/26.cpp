d7214f42b33a87e9dfbf19fe77bb9a1d4f823c9d
CarryKim <137850094+CarryKim@users.noreply.github.com>
2024-01-04 16:13:02 +0900

    26.cpp

---
+++ C++/26.cpp
@@ -0,0 +1,14 @@
+// 26. Remove Duplicates from Sorted Array
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int cnt = 1;
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i] != nums[i-1])
+                nums[cnt++] = nums[i]; 
+        }
+        return cnt;
+    }
+};
