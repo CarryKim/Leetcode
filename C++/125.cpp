6013137d6a9fe040f6faf507ba3d4442d37d7e83
CarryKim <137850094+CarryKim@users.noreply.github.com>
2024-01-05 14:37:02 +0900

    125.cpp

--- C++/125.cpp
+++ C++/125.cpp
@@ -1,3 +1,25 @@
 // 125. Valid Palindrome
 // https://leetcode.com/problems/valid-palindrome/
 // Authored by CarryKim
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string tmp = "";
+        for(auto c : s) {
+            if(isalnum(c))
+                tmp += tolower(c);
+        }
+        int start = 0;
+        int end = tmp.size() - 1;
+        while(start <= end) {
+            if(tmp[start] != tmp [end])
+                return false;
+            else {
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+};
