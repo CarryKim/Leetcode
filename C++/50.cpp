c6d0fe781f304890403b74ed7c2574acdaedc4fa
CarryKim <137850094+CarryKim@users.noreply.github.com>
2024-01-17 10:13:06 +0900

    50.cpp

---
+++ C++/50.cpp
@@ -0,0 +1,10 @@
+// 50. Pow(x, n)
+// https://leetcode.com/problems/powx-n/
+// Authored by CarryKim
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x,n);
+    }
+};
